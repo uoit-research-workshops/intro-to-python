97ad9403980de1f83a6dfaf87d53adf1fcacbd9e
gclen <graham.clendenning@uoit.net>
2015-07-23 15:28:49 -0400

    Initial add of some examples

---
+++ examples/fun_with_types.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+#This is an int
+a = 1
+#This is a float
+b = 65.2
+#This is a string
+c = 'banana'
+#This is a boolean
+d = True
+
+#This is an example of a list
+mylist = [a, b, c, d]
+
+#Looping over a list
+for item in mylist:
+
+    #Lets check that our data types are correct     
+    print item, type(item)
+
+    #Compare items in the list to something
+    if item == 'banana':
+        print 'This is a banana'
+
+    else:
+        print 'This is not a banana'
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ examples/hello_world.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+#Print hello world to the screen
+print 'Hello world!'
+
+
